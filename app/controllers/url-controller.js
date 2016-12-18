35c6cbb9076672594e87ae622fcd8c7337353a4f
DucNguyen <vanducld@gmail.com>
2016-12-18 11:12:03 +0700

    correct counter issue, by using only one row

--- app/controllers/url-controller.js
+++ app/controllers/url-controller.js
@@ -2,46 +2,69 @@ var Url = require('mongoose').model('Url');
 var Counter = require('mongoose').model('Counter');
 var base58 = require('./base58');
 exports.create = function(req, res, next){
-	Counter.find({}).sort('-date').limit(10).exec(function(err, count){
-		console.log("cpinter : ",count);
-	    if(count && count.length == 0){
-	    	var saveCount = new Counter({
-	    		seq: 1
-	    	});
-	    	var url = new Url(req.body);	
-			url.shortened = base58.encode(1);
-			url.createdAt = new Date();
-	    	saveCount.save(function(){
-	    		url.save(function(err){
+	if(req.body.custom){
+		console.log("create url with custom");
+		Url.findOne({
+			shortened: req.body.custom
+		}, function(err, url) {
+			console.log("error : ",err);
+			if (!url) {
+				var url = new Url(req.body);
+				url.shortened =  req.body.custom;
+				url.createdAt = new Date();
+				url.save(function(err){
 					if(err){
 						console.log("Can not save url :", req.body);
 						next(err);
 					}else{
 						res.json(url);
 					}
-				})
-	    	});
-	    }else{	    	
-	    	var url = new Url(req.body);
-	    	var newCounter = count[count.length - 1].seq + 1;	
-			url.shortened = base58.encode(newCounter);
-			url.createdAt = new Date();
+				});
+			} else {
+				res.statusCode = 400;
+				res.end();
+			}
+		});
+	}else{
+		Counter.findOne({},function(err, count){
+		    if(!count){
+		    	var saveCount = new Counter({
+		    		seq:1
+		    	});
+		    	var url = new Url(req.body);	
+				url.shortened = base58.encode(1);
+				url.createdAt = new Date();
+		    	saveCount.save(function(){
+		    		url.save(function(err){
+						if(err){
+							console.log("Can not save url :", req.body);
+							next(err);
+						}else{
+							res.json(url);
+						}
+					})
+		    	});
+		    }else{	    	
+		    	var url = new Url(req.body);
+		    	var newCounter = count.seq + 1;	
+				url.shortened = base58.encode(newCounter);
+				url.createdAt = new Date();
 
-			var saveCount = new Counter({
-	    		seq: newCounter
-	    	});
-	    	saveCount.save(function(){
-	    		url.save(function(err){
-					if(err){
-						console.log("Can not save url :", req.body);
-						next(err);
-					}else{
-						res.json(url);
-					}
-				})
-	    	});
-	    }
-	});
+				var saveCount = count;
+				saveCount.seq = newCounter;
+		    	saveCount.save(function(){
+		    		url.save(function(err){
+						if(err){
+							console.log("Can not save url :", req.body);
+							next(err);
+						}else{
+							res.json(url);
+						}
+					})
+		    	});
+		    }
+		});
+	}	
 	
 }
 exports.find = function(req, res, next) {
@@ -75,7 +98,7 @@ exports.deleteUrl = function(req, res) {
 	    	res.statusCode = 200;          
 	    }
 	    else {	       
-	    	res.statusCode = 500;      
+	    	res.statusCode = 400;      
 	    }
 	    res.end();
 	});
