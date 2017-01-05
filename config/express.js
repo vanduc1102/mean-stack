78c1d2a8349334c8836961fb96efb0267034b0e1
DucNguyen <vanducld@gmail.com>
2017-01-05 23:36:14 +0700

    add inital for passport

--- config/express.js
+++ config/express.js
@@ -4,6 +4,7 @@ var express = require("express"),
 	bodyParser = require("body-parser"),
 	methodOverride = require('method-override'),
 	session = require('express-session'),
+	passport = require('passport'),
 	config = require('./config');
 
 module.exports = function (){
@@ -31,6 +32,9 @@ module.exports = function (){
 
 	app.set('views','./app/views');
 	app.set('view engine', 'ejs');
+
+	app.use(passport.initialize());
+	app.use(passport.session());
 	//app.use(basicAuth);
 	require('../app/routers/router.js')(app);
 
